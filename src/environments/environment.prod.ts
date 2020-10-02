e771ec73c395535e85c6e70b44b8e181f483a830
Lidia Real <lidiarq96@gmail.com>
2020-10-02 11:23:06 +0200

    Environment listo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
-// TODO: Cambiar el url de los servicios en prod
 
 export const environment = {
   production: true,
-  base_url: 'http://localhost:3000/api'
+  base_url: 'https://adminpro-backend-lidia.herokuapp.com/api'
 };
